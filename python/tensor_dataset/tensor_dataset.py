b5f53f3351f06b68e4079632af56b2f789a0e2f2
LuoBin <2646887443@qq.com>
2025-04-14 14:35:18 +0800

    dataset len

--- python/tensor_dataset/tensor_dataset.py
+++ python/tensor_dataset/tensor_dataset.py
@@ -14,6 +14,12 @@ for i, (X, y) in enumerate(data_iter):
     print(f'X: {X}')
     print(f'y: {y}\n')
 
+for f, l in dataset:
+    print(f, l)
+
+for i in range(len(dataset)):
+    print(dataset[i][0], dataset[i][1])
+
 X = torch.arange(180.0).reshape(10, 2, 3, 3)
 y = torch.arange(10.0)
 print(f'X: {X}')
@@ -26,3 +32,9 @@ for i, (X, y) in enumerate(data_iter):
     print(f'\ni: {i}')
     print(f'X: {X}')
     print(f'y: {y}\n')
+
+for f, l in dataset:
+    print(f, l)
+
+for i in range(len(dataset)):
+    print(dataset[i][0], dataset[i][1])
