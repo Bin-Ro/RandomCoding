ef17eeda9de4551b217d432d2b91761da533cee0
LuoBin <2646887443@qq.com>
2025-04-24 15:41:14 +0800

    my_argmax

--- python/my_argmax/my_argmax.py
+++ python/my_argmax/my_argmax.py
@@ -3,3 +3,8 @@ import torch
 x = torch.tensor([[0, 1, 0], [1, 0, 0], [0, 0, 1]])
 print(f'x: {x}')
 print(f'x.argmax(dim=1): {x.argmax(dim=1)}')
+
+y = torch.tensor([[0, 0, 1, 0, 0]])
+print(f'y: {y}')
+print(f'y.argmax(dim=1): {y.argmax(dim=1)}')
+print(f'y.argmax(dim=1).reshape(1): {y.argmax(dim=1).reshape(1)}')
